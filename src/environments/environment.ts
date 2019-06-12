db073e30dfd28c8630f48677e279f707aa49d6a5
IsaiasPC <isamed92@gmail.com>
2019-06-11 21:23:58 -0300

    add firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAxHvom9PcEGyUByks4EGlIy8pJLqGPph0",
+    authDomain: "firechat-e1290.firebaseapp.com",
+    databaseURL: "https://firechat-e1290.firebaseio.com",
+    projectId: "firechat-e1290",
+    storageBucket: "firechat-e1290.appspot.com",
+    messagingSenderId: "422358884487",
+    appId: "1:422358884487:web:2b29a5d4081ab39c"
+  }
 };
 
 /*
